c80339ff3b9b3f4864a949fab76b6d129857542b
Rpan4853 <raymondpan12345@gmail.com>
2022-04-01 17:06:55 -0400

    Created basic Preview component and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./modules/NavBar";
+import Preview from "./modules/Preview";
 
 const App = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <div className="App-previewContainer">
+        <Preview type="Events" />
+        <Preview type="Updates" />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/modules/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Preview.css";
+
+const Preview = ({ type }) => {
+  return (
+    <div className="Preview-container">
+      <h1>{type} Preview</h1>
+    </div>
+  );
+};
+
+export default Preview;
