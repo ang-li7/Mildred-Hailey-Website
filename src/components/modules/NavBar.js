83587a1c3d79d889a127f16bb19649be9265aaf2
Rpan4853 <raymondpan12345@gmail.com>
2022-04-01 16:45:20 -0400

    Added styling to NavBar

--- src/components/modules/NavBar.js
+++ src/components/modules/NavBar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>Mildred Hailey</h1>
-      <h1>Events</h1>
-      <h1>Updates</h1>
-      <h1>About</h1>
-      <h1>Partner Login</h1>
+    <div className="NavBar-container">
+      <div className="NavBar-linkContainer">
+        <h1>Mildred Hailey</h1>
+        <h1>Events</h1>
+        <h1>Updates</h1>
+        <h1>About</h1>
+        <h1>Partner Login</h1>
+      </div>
     </div>
   );
 };
