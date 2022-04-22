c413687b3cf7af57b0a7082f68a4d80f800bca54
Rpan4853 <raymondpan12345@gmail.com>
2022-04-22 14:57:16 -0400

    Set up firebase

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
